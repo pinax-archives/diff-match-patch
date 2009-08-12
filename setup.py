29d8d9dbcfecca5d8a790c8d9f3026810286b7a7
Brian Rosner <brosner@gmail.com>
2009-08-11 19:13:47 -0600

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='diff-match-patch',
-    version='20090110',
+    version='20090804',
     author='Neil Fraser',
     url='http://code.google.com/p/google-diff-match-patch/',
     py_modules=['diff_match_patch'],
